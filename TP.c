033bf894027fc7e898f603e154512149cfcc4021
Bigodeiro <enzobigodeiro@proton.me>
2024-02-05 21:48:00 -0300

    é isso

--- TP.c
+++ TP.c
@@ -65,7 +65,7 @@ void* alocMat(int y, int x, int typeSize)
     return mat;
 }
 
-void printaNonograma(char **mat, coord tamMat, coord espacamentoMax, int **xCabecalho, int **yCabecalho)
+void printaNonograma(char **mat, coord tamMat, coord espacamentoMax, int **linCabecalho, int **colCabecalho)
 {
     //LogicaPrintaNonograma.png
     //!Parte 1
@@ -75,9 +75,9 @@ void printaNonograma(char **mat, coord tamMat, coord espacamentoMax, int **xCabe
 
         for (int j = 0; j < tamMat.x; j++)
         {
-            if (yCabecalho[j][0] > espacamentoMax.y - i - 1)
+            if (colCabecalho[j][0] > espacamentoMax.y - i - 1)
             {
-                printf("%2d", yCabecalho[j][yCabecalho[j][0] - espacamentoMax.y + i + 1]);
+                printf("%2d", colCabecalho[j][colCabecalho[j][0] - espacamentoMax.y + i + 1]);
             }else printaChar(2, ' ');
 
             printaChar(1, ' ');
@@ -99,11 +99,11 @@ void printaNonograma(char **mat, coord tamMat, coord espacamentoMax, int **xCabe
     //!Parte 3
     for (int i = 0; i < tamMat.y; i++)
     {
-        printaChar((espacamentoMax.x - xCabecalho[i][0]) * 3, ' ');
+        printaChar((espacamentoMax.x - linCabecalho[i][0]) * 3, ' ');
 
-        for (int j = 0; j < xCabecalho[i][0]; j++)
+        for (int j = 0; j < linCabecalho[i][0]; j++)
         {
-            printf("%2d ", xCabecalho[i][j+1]);
+            printf("%2d ", linCabecalho[i][j+1]);
         }
 
 
@@ -142,13 +142,13 @@ int alteraNonograma(char **mat, coord tamMat, coord pos, char c, char *buffer)
     return 0;
 }
 
-void salvaArquivo(char *path, char **mat, coord tamMat, int **xCabecalho, int **yCabecalho)
+void salvaArquivo(char *caminho, char **mat, coord tamMat, int **linCabecalho, int **colCabecalho)
 {
-    FILE *arqNonograma = fopen(path, "w");
+    FILE *arqNonograma = fopen(caminho, "w");
     
     if (arqNonograma == NULL) 
     {
-        arqNonograma = fopen(path, "a");
+        arqNonograma = fopen(caminho, "a");
         if (arqNonograma == NULL) 
         {
             printf("Erro ao criar o arquivo.\n");
@@ -160,22 +160,22 @@ void salvaArquivo(char *path, char **mat, coord tamMat, int **xCabecalho, int **
 
     for (int i = 0; i < tamMat.y; i++)
     {
-        fprintf(arqNonograma, "%d ", xCabecalho[i][0]);
+        fprintf(arqNonograma, "%d ", linCabecalho[i][0]);
 
-        for (int j = 0; j < xCabecalho[i][0]; j++)
+        for (int j = 0; j < linCabecalho[i][0]; j++)
         {
-            fprintf(arqNonograma, "%d ", xCabecalho[i][j+1]);
+            fprintf(arqNonograma, "%d ", linCabecalho[i][j+1]);
         }
         fprintf(arqNonograma, "\n");
     }
 
     for (int i = 0; i < tamMat.x; i++)
     {
-        fprintf(arqNonograma, "%d ", yCabecalho[i][0]);
+        fprintf(arqNonograma, "%d ", colCabecalho[i][0]);
 
-        for (int j = 0; j < yCabecalho[i][0]; j++)
+        for (int j = 0; j < colCabecalho[i][0]; j++)
         {
-            fprintf(arqNonograma, "%d ", yCabecalho[i][j+1]);
+            fprintf(arqNonograma, "%d ", colCabecalho[i][j+1]);
         }
         fprintf(arqNonograma, "\n");
     }
@@ -243,11 +243,8 @@ int comparaVetMat_Cabecalho(char* vetMat, int tamVetMat, int* vetCabecalhoGabari
             }
         }
 
-        printf("N real de grupos: %d\n", nGruposVerdadeiro);
-
         if (nGruposVerdadeiro > vetCabecalhoGabarito[0])
         {
-            printf("chego");
             free(vetCabecalhoMat);
             return 2;
         }else
@@ -302,7 +299,7 @@ char * getMatVet(char **mat, coord tamMat, int isCol, int index)
     return vet;
 }
 
-int checaVitoria(char **mat, coord tamMat, int **xCabecalho, int **yCabecalho)
+int checaVitoria(char **mat, coord tamMat, int **linCabecalho, int **colCabecalho)
 {
     char *aux = NULL;
     int resolvido = 1;
@@ -310,7 +307,7 @@ int checaVitoria(char **mat, coord tamMat, int **xCabecalho, int **yCabecalho)
     for (int i = 0; i < tamMat.y; i++)
     {
         aux = getMatVet(mat, tamMat, false, i);
-        if (comparaVetMat_Cabecalho(mat[i], tamMat.x, xCabecalho[i]) != 0)
+        if (comparaVetMat_Cabecalho(mat[i], tamMat.x, linCabecalho[i]) != 0)
         {
             resolvido = 0;
             free(aux);
@@ -324,23 +321,8 @@ int checaVitoria(char **mat, coord tamMat, int **xCabecalho, int **yCabecalho)
         for (int i = 0; i < tamMat.x; i++)
         {
             aux = getMatVet(mat, tamMat, true, i);
-            if (comparaVetMat_Cabecalho(aux, tamMat.y, yCabecalho[i]) != 0)
+            if (comparaVetMat_Cabecalho(aux, tamMat.y, colCabecalho[i]) != 0)
             {
-                //print aux
-                printf("Erro na coluna %d\n", i);
-                for (int j = 0; j < tamMat.y; j++)
-                { 
-                    printf("%c", aux[j]);
-                }
-                printf("\n");
-                
-                //print cabecalho y
-                for (int j = 0; j < yCabecalho[i][0]; j++)
-                {
-                    printf("%d ", yCabecalho[i][j+1]);
-                }
-                printf("\n");
-                
                 free(aux);
                 resolvido = 0;
                 break;
@@ -352,10 +334,10 @@ int checaVitoria(char **mat, coord tamMat, int **xCabecalho, int **yCabecalho)
     return resolvido;
 }
 
-int checaJogada(char **mat, coord tamMat, int **xCabecalho, int **yCabecalho, coord pos, char c)
+int checaJogada(char **mat, coord tamMat, int **linCabecalho, int **colCabecalho, coord pos, char c)
 {
     char* aux = getMatVet(mat, tamMat, false, pos.y);
-    if (comparaVetMat_Cabecalho(mat[pos.y], tamMat.x, xCabecalho[pos.y]) == 2)
+    if (comparaVetMat_Cabecalho(mat[pos.y], tamMat.x, linCabecalho[pos.y]) == 2)
     {
         free(aux);
         return 4;
@@ -363,7 +345,7 @@ int checaJogada(char **mat, coord tamMat, int **xCabecalho, int **yCabecalho, co
     free(aux);
 
     aux = getMatVet(mat, tamMat, true, pos.x);
-    if (comparaVetMat_Cabecalho(aux, tamMat.y, yCabecalho[pos.x]) == 2)
+    if (comparaVetMat_Cabecalho(aux, tamMat.y, colCabecalho[pos.x]) == 2)
     {
         free(aux);
         return 4;
@@ -373,40 +355,103 @@ int checaJogada(char **mat, coord tamMat, int **xCabecalho, int **yCabecalho, co
     return 0;
 }
 
+void marcaLinha(char **mat, coord tamMat, coord inicio, coord fim, char c)
+{
+    //either inicio.x == fim.x or inicio.y == fim.y
+    if (inicio.x == fim.x)
+    {
+        for (int i = inicio.y; i <= fim.y; i++)
+        {
+            mat[i][inicio.x] = c;
+        }
+    }else
+    {
+        for (int i = inicio.x; i <= fim.x; i++)
+        {
+            mat[inicio.y][i] = c;
+        }
+    }
+} 
+//A function that completes the nonogram correctly
+int resolverNonograma(char **mat, coord tamMat, int **linCabecalho, int **colCabecalho)
+{
+    for (int i = 0; i < tamMat.y; i++)
+    {
+        for (int k = 1; k <= linCabecalho[i][0]; k++)
+        {
+            int dif = tamMat.x - linCabecalho[i][k];
+            int inicio = dif;
+            int fim = tamMat.x - dif - 1;
+
+            marcaLinha(mat, tamMat, (coord){inicio, i}, (coord){fim, i}, 'x');
+
+            if (inicio <= fim && linCabecalho[i][0] == 1)
+            {
+                for (int j = 0; j < tamMat.x; j++)
+                {
+                    if (mat[i][j] != 'x') mat[i][j] = '-';
+                }
+            }
+        }   
+    }
+    
+    for (int i = 0; i < tamMat.x; i++)
+    {
+        for (int k = 1; k <= colCabecalho[i][0]; k++)
+        {
+            int dif = tamMat.y - colCabecalho[i][k];
+            int inicio = dif;
+            int fim = tamMat.y - dif - 1;
+
+            marcaLinha(mat, tamMat, (coord){i, inicio}, (coord){i, fim}, 'x');
+
+            if (inicio <= fim && colCabecalho[i][0] == 1)
+            {
+                for (int j = 0; j < tamMat.y; j++)
+                {
+                    if (mat[j][i] != 'x') mat[j][i] = '-';
+                }
+            }
+        }   
+    }
+    
+    return 0;
+}
+
 int main (int argc, char *argv[])
 {
     //? Criacao de variaveis
     int boasVindasFlag = 1;
-    char path[260];
-    strcpy(path, argv[1]);
+    char caminho[260];
+    strcpy(caminho, argv[1]);
     coord espacamentoMax = {0, 0};
     coord tamMat;
-    int **xCabecalho;
-    int **yCabecalho;
+    int **linCabecalho;
+    int **colCabecalho;
     int continuar = 1;
     char** mat;
     char input[10];
     int recado = 0; //? 0 -> sem recado, 1 -> recado de comando invalido, 2 -> recado de coordenada invalida, 3 -> recado lista de comandos
 
     //? Leitura de arquivo
-    FILE *arqNonograma = fopen(path, "r");
+    FILE *arqNonograma = fopen(caminho, "r");
     fscanf(arqNonograma, "%d %d", &tamMat.y, &tamMat.x);
 
     //? Alocacao dos cabecalhos
-    xCabecalho = malloc(tamMat.y * sizeof(int*));
-    yCabecalho = malloc(tamMat.x * sizeof(int*));
+    linCabecalho = malloc(tamMat.y * sizeof(int*));
+    colCabecalho = malloc(tamMat.x * sizeof(int*));
 
     //? Ler cabecalho x
     for (int i = 0; i < tamMat.y; i++)
     {
         int aux;
         fscanf(arqNonograma, "%d", &aux);
-        xCabecalho[i] = malloc((aux + 1) * sizeof(int));
-        xCabecalho[i][0] = aux;
+        linCabecalho[i] = malloc((aux + 1) * sizeof(int));
+        linCabecalho[i][0] = aux;
 
         for (int j = 0; j < aux; j++)
         {
-            fscanf(arqNonograma, "%d", &xCabecalho[i][j+1]);
+            fscanf(arqNonograma, "%d", &linCabecalho[i][j+1]);
         }
 
         espacamentoMax.x = aux > espacamentoMax.x ? aux : espacamentoMax.x;
@@ -417,12 +462,12 @@ int main (int argc, char *argv[])
     {
         int aux;
         fscanf(arqNonograma, "%d", &aux);
-        yCabecalho[i] = malloc((aux + 1) * sizeof(int));
-        yCabecalho[i][0] = aux;
+        colCabecalho[i] = malloc((aux + 1) * sizeof(int));
+        colCabecalho[i][0] = aux;
 
         for (int j = 0; j < aux; j++)
         {
-            fscanf(arqNonograma, "%d", &yCabecalho[i][j+1]);
+            fscanf(arqNonograma, "%d", &colCabecalho[i][j+1]);
         }
 
         espacamentoMax.y = aux > espacamentoMax.y ? aux : espacamentoMax.y;
@@ -448,124 +493,125 @@ int main (int argc, char *argv[])
         }
     }
 
-    //! Codigo a ser executado em loop
-    while (continuar)
-    {
-        char cordenadas[260] = {0, 0};
-        char pathCaminho[260];
+    resolverNonograma(mat, tamMat, linCabecalho, colCabecalho);
+    printaNonograma(mat, tamMat, espacamentoMax, linCabecalho, colCabecalho);
 
-        if (boasVindasFlag)
-        {
-            printaChar(20, '\n');
-            printf("Bem vindo ao Nonograma!");
-            printaComandos();
-            boasVindasFlag = 0;
-        }else printaChar(20, '\n');
-        //Recado a ser dado ao jogador entre um resultado e outro
-        switch (recado)
-        {
-        case 1:
-            printf("Comando invalido, digite novamente\n");
-            break;
+    // //! Codigo a ser executado em loop
+    // while (continuar)
+    // {
+    //     char cordenadas[260] = {0, 0};
+    //     char strCaminho[260];
+
+    //     if (boasVindasFlag)
+    //     {
+    //         printaChar(20, '\n');
+    //         printf("Bem vindo ao Nonograma!");
+    //         printaComandos();
+    //         boasVindasFlag = 0;
+    //     }else printaChar(20, '\n');
+    //     //Recado a ser dado ao jogador entre um resultado e outro
+    //     switch (recado)
+    //     {
+    //     case 1:
+    //         printf("Comando invalido, digite novamente\n");
+    //         break;
         
-        case 2:
-            printf("Coordenada invalida, digite novamente\n");
-            break;
+    //     case 2:
+    //         printf("Coordenada invalida, digite novamente\n");
+    //         break;
         
-        case 3:
-            printaComandos();
-            break;
-        case 4:
-            printf("Você infligiu as regras do nonograma, portanto o ultimo movimento foi desfeito\n");
-            break;
+    //     case 3:
+    //         printaComandos();
+    //         break;
+    //     case 4:
+    //         printf("Você infligiu as regras do nonograma, portanto o ultimo movimento foi desfeito\n");
+    //         break;
+    //     case 5:
+    //         printf("Nonograma resolvido! (ou quase isso)\n");
+    //         break;
         
-        default:
-            break;
-        }
-        printaNonograma(mat, tamMat, espacamentoMax, xCabecalho, yCabecalho);
+    //     default:
+    //         break;
+    //     }
+    //     printaNonograma(mat, tamMat, espacamentoMax, linCabecalho, colCabecalho);
         
-        printf("\nDigite um comando: ");
+    //     printf("\nDigite um comando: ");
         
-        fflush(stdin);
-        scanf("%s", input);
-        fflush(stdin);
+    //     fflush(stdin);
+    //     scanf("%s", input);
+    //     fflush(stdin);
 
-        recado = 0;
-        switch (interpretaInput(input))
-        {
-        case 'v':
-            fflush(stdin);
-            scanf("%s", cordenadas);
-            fflush(stdin);
+    //     recado = 0;
+    //     switch (interpretaInput(input))
+    //     {
+    //     case 'v':
+    //         fflush(stdin);
+    //         scanf("%s", cordenadas);
+    //         fflush(stdin);
 
-            printf("tam input: %d\n", strlen(cordenadas));
+    //         if (strlen(cordenadas) > 2)
+    //         {
+    //             recado = 1;
+    //         }            
 
-            if (strlen(cordenadas) > 2)
-            {
-                recado = 1;
-            }            
+    //         int y = letra2int(cordenadas[0]);
+    //         int x = letra2int(cordenadas[1]);
+    //         char buffer;
 
-            int y = letra2int(cordenadas[0]);
-            int x = letra2int(cordenadas[1]);
-            char buffer;
-
-            if (recado == 0)
-            {
-                recado = alteraNonograma(mat, tamMat, (coord){ x, y }, input[0], &buffer);
-            }
+    //         if (recado == 0)
+    //         {
+    //             recado = alteraNonograma(mat, tamMat, (coord){ x, y }, input[0], &buffer);
+    //         }
         
-            //*Checa se a jogada inflige regra
-            if (recado == 0)//Se nao tiver erros
-            {
-                recado = checaJogada(mat, tamMat, xCabecalho, yCabecalho, (coord){ x, y }, input[0]);
+    //         //*Checa se a jogada inflige regra
+    //         if (recado == 0)//Se nao tiver erros
+    //         {
+    //             recado = checaJogada(mat, tamMat, linCabecalho, colCabecalho, (coord){ x, y }, input[0]);
 
-                if (recado == 4)//Se a jogada tiver infligido regra
-                {
-                    //desfaz a alteracao
-                    alteraNonograma(mat, tamMat, (coord){ x, y }, buffer, &buffer);
-                }
-            }
+    //             if (recado == 4)//Se a jogada tiver infligido regra
+    //             {
+    //                 //desfaz a alteracao
+    //                 alteraNonograma(mat, tamMat, (coord){ x, y }, buffer, &buffer);
+    //             }
+    //         }
 
-            //*checa se o nonograma foi resolvido
-            if (recado == 0)//Se nao tiver erros
-            {
-                if (checaVitoria(mat, tamMat, xCabecalho, yCabecalho))
-                {
-                    beep(); //:)
-                    printf("Parabens, voce resolveu o nonograma!\n");
-                    printf("\nDeseja jogar continuar? (s/n): ");
-                    char continuarChar;
-                    scanf("%c", &continuarChar);
-                    if (continuarChar == 'n' || continuarChar == 'N') continuar = 0;
-                }
-            }
+    //         //*checa se o nonograma foi resolvido
+    //         if (recado == 0)//Se nao tiver erros
+    //         {
+    //             if (checaVitoria(mat, tamMat, linCabecalho, colCabecalho))
+    //             {
+    //                 beep(); //:)
+    //                 printf("Parabens, voce resolveu o nonograma!\n");
+    //             }
+    //         }
 
-            break;
+    //         break;
 
-        case 'r':
-            printf("Resolver\n");
-            break;
+    //     case 'r':
+    //         resolverNonograma(mat, tamMat, linCabecalho, colCabecalho);
+    //         recado = 5;
+    //         break;
 
-        case 'q':
-            continuar = 0;
-            break;
+    //     case 'q':
+    //         continuar = 0;
+    //         break;
 
-        case 's':
-            scanf("%s", pathCaminho);
-            salvaArquivo(pathCaminho, mat, tamMat, xCabecalho, yCabecalho);
-            printf("Nonograma salvo com sucesso!\n");
-            break;
+    //     case 's':
+    //         scanf("%s", strCaminho);
+    //         salvaArquivo(strCaminho, mat, tamMat, linCabecalho, colCabecalho);
+    //         printf("Nonograma salvo com sucesso!\n");
+    //         break;
         
-        case 'c':
-            recado = 3;
-            break;
+    //     case 'c':
+    //         recado = 3;
+    //         break;
 
-        default:
-            recado = 1;
-            break;
+    //     default:
+    //         recado = 1;
+    //         break;
 
-        }
-    }
+    //     }
+    // }
 
     //? Liberacao de memoria
     for (int i = 0; i < tamMat.y; i++)
@@ -576,15 +622,15 @@ int main (int argc, char *argv[])
 
     for (int i = 0; i < tamMat.y; i++)
     {
-        free(xCabecalho[i]);
+        free(linCabecalho[i]);
     }
-    free(xCabecalho);
+    free(linCabecalho);
 
     for (int i = 0; i < tamMat.x; i++)
     {
-        free(yCabecalho[i]);
+        free(colCabecalho[i]);
     }
-    free(yCabecalho);
+    free(colCabecalho);
 
     fclose(arqNonograma);
     return 0;
